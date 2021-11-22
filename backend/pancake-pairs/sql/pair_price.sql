741b52842a40de65e93132c884bc27992250444b
Sebastiaan Raven <basraven@gmail.com>
2021-11-22 23:17:05 +0100

    Adding hypertable definition

--- backend/pancake-pairs/sql/pair_price.sql
+++ backend/pancake-pairs/sql/pair_price.sql
@@ -8,3 +8,5 @@ CREATE TABLE assetvar_data.pair_price (
   stableCoin         CHAR(42)          NOT NULL,
   PRIMARY KEY (currentTime, pairAddress)
 );
+
+SELECT create_hypertable('assetvar_data.pair_price', 'currenttime', migrate_data => TRUE, chunk_time_interval => INTERVAL '1 day');
