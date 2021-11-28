017bd5e291eef92d903cccfeb7f4a3b23c04df2b
Sebastiaan Raven <basraven@gmail.com>
2021-11-28 14:05:54 +0100

    new structure

---
+++ backend/pancake/.examples/historical_price.py
@@ -0,0 +1,12 @@
+from web3 import Web3
+
+web3 = Web3(Web3.HTTPProvider('https://data-seed-prebsc-1-s1.binance.org:8545'))
+abi = '[{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
+addr = '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE'
+contract = web3.eth.contract(address=addr, abi=abi)
+
+#  Valid roundId must be known. They are NOT incremental.
+validRoundId = 18446744073709554130
+
+historicalData = contract.functions.getRoundData(validRoundId).call()
+print(historicalData)
